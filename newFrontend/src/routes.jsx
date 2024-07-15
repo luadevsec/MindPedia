a6a524ef3f8b8acf9ac68c88878072c496c95362
Lunna Cipher <lunna.devsec@gmail.com>
2024-07-15 00:43:34 -0300

    definição arquitetural
    criação e definição do routes e do strict mode, alem de uma pagina 404

---
+++ newFrontend/src/routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from '@pages/NotFound';
+
+
+
+
+const AppRoutes = () => {
+    return (
+      <Router>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    );
+  };
+
+export default AppRoutes;
