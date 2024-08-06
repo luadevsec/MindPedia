fb113660dadbd8bc11db21e4fa46de2c59b8e7cc
Lunna Cipher <lunna.devsec@gmail.com>
2024-08-06 19:35:50 -0300

    🏭união do menu

---
+++ frontend/src/utils/date.ts
@@ -0,0 +1,10 @@
+const formatTime = (date : any) => {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    return `${day}/${month} ${hours}:${minutes}`;
+};
+
+export { formatTime };
