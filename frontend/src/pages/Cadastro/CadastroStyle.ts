55f45069965d71fead5a808ab04eac55745ef0f2
Lunna Cipher <lunna.devsec@gmail.com>
2024-09-24 18:38:52 -0300

    🎨isolamento do css <Cadastro>

---
+++ frontend/src/pages/Cadastro/CadastroStyle.ts
@@ -0,0 +1,26 @@
+/* CampoCadastro.css */
+
+.cadastro-field {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.cadastro-field label {
+    font-size: 1em;
+    color: #333;
+}
+
+.cadastro-field input {
+    padding: 10px;
+    font-size: 1em;
+    border: 1px solid #bdc3c7;
+    border-radius: 5px;
+    background-color: white;
+    color: #333;
+}
+
+.cadastro-field input:focus {
+    outline: none;
+    border-color: #2980b9;
+}
