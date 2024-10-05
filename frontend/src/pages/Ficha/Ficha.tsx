ad7b56df2101ed7f888b2dc538d5de15efbbebb2
Lunna Cipher <lunna.devsec@gmail.com>
2024-10-04 21:10:20 -0300

    💩 oq q eu fix
    adasdas

--- frontend/src/pages/Ficha/Ficha.tsx
+++ frontend/src/pages/Ficha/Ficha.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+//dsasdas
 
 
 function Campo ({ titulo, conteudo }: { titulo: string, conteudo: string | null }) {
