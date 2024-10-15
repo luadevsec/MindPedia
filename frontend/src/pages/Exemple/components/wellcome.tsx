62b4132c153ab0e775b8670178fc4343987089a4
Lunna Cipher <lunna.devsec@gmail.com>
2024-10-15 01:37:13 -0300

    💄style do wellcome
    implementando o estilo do component wellcome com styled component

--- frontend/src/pages/Exemple/components/wellcome.tsx
+++ frontend/src/pages/Exemple/components/wellcome.tsx
@@ -1,11 +1,18 @@
+import styled from 'styled-components';
 
+const WellcomeContainer = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 24px;
+  color: #333;
+`;
 
 const Wellcome: React.FC = () => {
   return (
-    <div>
+    <WellcomeContainer>
       <h1>Bem-vindo à Página Secreta!</h1>
       <p>Aproveite o contador abaixo 🎉</p>
-    </div>
+    </WellcomeContainer>
   );
 };
 
