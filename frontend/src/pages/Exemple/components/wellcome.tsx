1febd3bc4fd154f200cede7f52e57060e8583d74
Lunna Cipher <lunna.devsec@gmail.com>
2024-10-15 01:28:00 -0300

    🚧 wellcome do secret
    criando o component do wellcome

---
+++ frontend/src/pages/Exemple/components/wellcome.tsx
@@ -0,0 +1,12 @@
+
+
+const Wellcome: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Página Secreta!</h1>
+      <p>Aproveite o contador abaixo 🎉</p>
+    </div>
+  );
+};
+
+export default Wellcome;
