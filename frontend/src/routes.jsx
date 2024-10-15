ff9640d8607d78a21e773bd5c08be8e0303b294c
Lunna Cipher <lunna.devsec@gmail.com>
2024-10-15 01:46:11 -0300

    🚀secret page
    adicionando a secret page ao sistema

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // pages
 import NotFound from './pages/NotFound';
 import Menu from './pages/Menu'
+import Secret from './pages/Exemple/Secret';
 
 
 
@@ -13,6 +14,7 @@ const AppRoutes = () => {
       <Router>
         <Routes>
           <Route path="/" element={<NotFound />} />
+          <Route path="/quack" element={<Secret />} />
           <Route path="/menu" element={<Menu/>}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
