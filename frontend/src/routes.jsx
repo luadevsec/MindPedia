7002a2bf826cb8fa9197f9a19ae6eb34afb7a72c
Lunna Cipher <lunna.devsec@gmail.com>
2024-10-15 02:53:22 -0300

    🚚mudando todas routes
    colocando os indexes novos

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // pages
-import NotFound from './pages/NotFound';
-import Menu from './pages/Menu'
-import Secret from './pages/Exemple/Secret';
-
-
-
+import NotFound from './pages/NotFound/NotFound'; // 404 page
+import Secret from './pages/Exemple/Secret'; // secret page easteregg
+import Menu from './pages/Menu/Menu'; // Menu page
+import Cadastro from './pages/Cadastro/Cadastro'; // Cadastro page
+import Ficha from './pages/Ficha/Ficha'; // Ficha page
+import Inicio from './pages/Inicio/Inicio'; // Inicio page
+import Atendimento from './pages/Atendimento/Atendimento'; // Atendimento page
 
 const AppRoutes = () => {
     return (
       <Router>
         <Routes>
-          <Route path="/" element={<NotFound />} />
+          <Route path="/" element={<Inicio />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/ficha" element={<Ficha />} />
+          <Route path="/atendimento" element={<Atendimento />} />
           <Route path="/quack" element={<Secret />} />
-          <Route path="/menu" element={<Menu/>}/>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
