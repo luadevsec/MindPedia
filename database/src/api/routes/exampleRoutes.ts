e6b56a005525bc7ccaf55113647fd90aafa2aeb8
DanyPLS <137570578+DanyPaes@users.noreply.github.com>
2024-12-10 19:32:54 -0300

    ficha

--- database/src/api/routes/exampleRoutes.ts
+++ database/src/api/routes/exampleRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import AgendarController from "../controllers/smpAgendarController";
+import AgendarController from "../controllers/exampleReq";
+import Ficha from "../controllers/exampleReq";
 
 const example = Router();
 
 example.post("/agendar", AgendarController.agendar);
-example.get("/ficha", AgendarController.ficha);
+example.post("/ficha", Ficha.ficha);
 
 
 export default example;
